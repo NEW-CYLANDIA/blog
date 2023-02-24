6022001d85d672144a7159aba4691333df83938f
iznaut <36773942+iznaut@users.noreply.github.com>
2023-02-24 10:09:42 -0600

    trigger build

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"title": "NEW CYLANDIA Public Library",
-	"url": "https://example.com/",
+	"url": "https://test.com/",
 	"language": "en",
 	"description": "A free knowledge base for gamedevs and other creatives"
 }
