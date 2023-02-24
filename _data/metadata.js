7908c4d382890e582d46e56c61cb8f3be7a82eb1
iznaut <36773942+iznaut@users.noreply.github.com>
2023-02-24 09:45:18 -0600

    about page, other template cleanup

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,6 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
+	"title": "NEW CYLANDIA Public Library",
 	"url": "https://example.com/",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
-	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
-	}
+	"description": "A free knowledge base for gamedevs and other creatives"
 }
